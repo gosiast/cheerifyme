9dedb6569150d92fbf50412397b97e9e76f05b7c
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-02-11 13:00:26 +0100

    updated styling of the IMG hero image

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -2,29 +2,28 @@ import React from "react";
 
 const Hero = () => {
 	return (
-		<main className="m-2">
+		<main className="m-0 p-0 bg-black">
 			<div className="container text-white">
-				<section className="col-span-5 place-self-center mt-4 lg:mt-0">
+				<section className="flex items-center justify-center">
 					<h1 className="text-4xl">
 						Welcome to <strong>Cheerify.me</strong>
 					</h1>
-					<div className="w-[250px] h-[250px] lg:w-[350px] lg:h-[400px] relative">
+					<div className="w-[100px] h-[100px] lg:w-[200px] lg:h-[200px] relative">
 						<img
-							className=""
 							src="/cheerifyme_logo.png"
 							alt=" logo with cartoon of a smiley face and headphones"
 						/>
 					</div>
 				</section>
-				<div>
-					<p>your go-to place, to make your day a little brighter ☀️</p>
+				<div className="p-2 flex items-center">
 					<p>
+						your go-to place, to make your day a little brighter ☀️
+						<br />
 						Imagine this: you&apos;re feeling a little down, in need of a
 						pick-me-up. Or maybe you're bursting with energy and just need the
 						perfect song to get your booty moving. Well, guess what? Cheerify.me
 						has got you covered! 🎶
-					</p>
-					<p>
+						<br />
 						Let me share something about myself. When I find myself experiencing
 						any emotion, from sadness to overjoy, I often turn on music and
 						start dancing (FYI – I&apos;m a big fan of bachata 💃). My room is
@@ -32,21 +31,23 @@ const Hero = () => {
 						They've helped me through both breakdowns and breakthroughs in life
 						✨ It got me thinking – why not create a place where everyone can
 						find that same kind of joy and inspiration? 💡
-					</p>{" "}
-					<p>See for yourself what&apos;s waiting for you here! 🤩</p>
-					<div className="flex items-center justify-center">
+						<br />
+						See for yourself what&apos;s waiting for you here! 🤩
+					</p>
+					<div className="">
 						<button className="text-white border-4 p-4">ENTER</button>
 					</div>
 				</div>
 			</div>
-			<section>
-				Remember, if you need additional support and/or you’re struggling with
-				mental health, it’s always best to contact a professional. Here you can
-				find some useful links https://checkpointorg.com/global/ or check in
-				your local community. You’re not alone, asking for help is a sign of
-				strength.
-			</section>
-			<footer>
+			<footer className="text-sm mt-2 text-center">
+				<section className="italic bg-slate-700 underline decoration-sky-500/30 p-2 rounded-sm">
+					If you need additional support and/or you’re struggling with mental
+					health, it’s always best to contact a professional. <br /> Here you
+					can find some useful links https://checkpointorg.com/global/ or check
+					in your local community. You’re not alone,{" "}
+					<strong>asking for help is a sign of strength.</strong>
+				</section>
+				<br />
 				This is an{" "}
 				<a href="https://github.com/gosiast/cheerifyme">open-source</a> project
 				and was created by{" "}
